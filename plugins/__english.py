72a71b63bf500adc58dfc71ef35652c2f0366d47
CrazyBossAbhishek <93928612+200920082007@users.noreply.github.com>
2022-03-03 06:43:45 +0530

    Update __english.py

--- plugins/__english.py
+++ plugins/__english.py
@@ -1,7 +1,7 @@
-from MashaRoBot import telethn as tbot
+from Alita.MashaRoBot import telethn as tbot
 import json
 import requests
-from MashaRoBot.events import register
+from Alita.MashaRoBot.events import register
 from telethon import *
 from telethon.tl import functions
 import os
