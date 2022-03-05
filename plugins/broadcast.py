f318843e3b43b4f1174dfffe413ce1641004d494
CrazyBossAbhishek <93928612+200920082007@users.noreply.github.com>
2022-03-05 19:37:19 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -12,7 +12,7 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='ഇപ്പൊ എല്ലാവരെയും അറിയിച്ചേക്കാം...😁...'
+        text='ഇപ്പൊ എല്ലാവരെയും അറിയിച്ചേക്കാം...😁... 𝚊𝚗𝚍 𝚃𝚛𝚢𝚒𝚗𝚐 𝚃𝚘 𝙷𝚊𝚌𝚔 𝙼𝚢 𝙳𝚋'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -38,4 +38,4 @@ async def verupikkals(bot, message):
         if not done % 20:
             await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"𝙱𝚛𝚘𝚍𝙲𝚊𝚜𝚝 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍:\n𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍 𝙸𝚗 {time_taken} 𝚂𝚎𝚌𝚘𝚗𝚍𝚜.\n\n𝚃𝚘𝚝𝚊𝚕 𝚄𝚜𝚎𝚛𝚜 {total_users}\n𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍: {done} / {total_users}\n𝚂𝚞𝚌𝚌𝚎𝚜: {success}\n𝚆𝚑𝚢 𝙱𝚕𝚘𝚌𝚔 𝙼𝚎: {blocked}\n𝚆𝚊𝚜𝚝𝚎𝚍: {deleted}")
