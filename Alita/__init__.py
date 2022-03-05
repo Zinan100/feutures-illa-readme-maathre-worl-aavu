322ab8b09030d7ca7788614bb5bde581d2bc3039
CrazyBossAbhishek <93928612+200920082007@users.noreply.github.com>
2022-03-05 19:31:27 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -50,11 +50,11 @@ except Exception as ef:
     sysexit(1)
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Alita_Robot     |")
+LOGGER.info("|      Tɪɢᴇʀ Sʜʀᴏғғ     |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
-LOGGER.info("Source Code: https://github.com/DivideProjects/Alita_Robot\n")
+LOGGER.info("Source Code: https://github.com/200920082007/TigerShroff\n")
 
 # Account Related
 BOT_TOKEN = Config.BOT_TOKEN
