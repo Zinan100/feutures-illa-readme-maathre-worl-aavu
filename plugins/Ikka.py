94a9593dc5957d540aff353c660483e047450363
CrazyBossAbhishek <93928612+200920082007@users.noreply.github.com>
2022-03-14 06:46:41 +0530

    Update Ikka.py

--- plugins/Ikka.py
+++ plugins/Ikka.py
@@ -4,7 +4,7 @@
 import random
 from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
-from plugins.neededshits.cust_p_filters import f_onw_fliter
+from plugins.helper_fuctions.cust_p_filters import f_onw_fliter
 
 
 IKKA_STRINGS = (
