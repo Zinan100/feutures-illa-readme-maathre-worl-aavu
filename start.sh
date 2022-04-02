a5569ae8ab675ef22285b7a021a72261289c3b81
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-02 11:18:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Zinan100/feutures-illa-readme-maathre-worl-aavu.git /copycat
+  git clone https://github.com/Zinan100/feutures-illa-readme-maathre-worl-aavu.git /feutures-illa-readme-maathre-worl-aavu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /copycat
+  git clone $UPSTREAM_REPO /feutures-illa-readme-maathre-worl-aavu
 fi
 cd /feutures-illa-readme-maathre-worl-aavu
 pip3 install -U -r requirements.txt
