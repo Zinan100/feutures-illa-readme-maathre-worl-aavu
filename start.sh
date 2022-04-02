7a18c66a9a56026c58ed1813a09c930b69352dd6
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-02 11:17:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/200920082007/TigerShroff.git /TigerShroff
+  git clone https://github.com/Zinan100/feutures-illa-readme-maathre-worl-aavu.git /copycat
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Tigershroff
+  git clone $UPSTREAM_REPO /copycat
 fi
-cd /Tigershroff
+cd /feutures-illa-readme-maathre-worl-aavu
 pip3 install -U -r requirements.txt
 echo "Starting TIGER Shroff....🔥"
 python3 bot.py
