d346150ee08a1b272c5aaecc1ec7bbc9544dc346
moviespot <97530806+Zinan100@users.noreply.github.com>
2022-04-02 13:20:49 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,7 +68,6 @@ async def start(client, message):
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
             parse_mode='html'
         )
         return
@@ -115,7 +114,6 @@ async def start(client, message):
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
             parse_mode='html'
         )
         return
